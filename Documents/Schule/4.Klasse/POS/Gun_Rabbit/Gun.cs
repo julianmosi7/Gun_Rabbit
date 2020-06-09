36790fc5f7eabae979bdf3c891eb97ccd25f9739
Julian <moshammer.ju@gmail.com>
2020-06-09 16:26:48 +0200

    to pc

---
+++ Documents/Schule/4.Klasse/POS/Gun_Rabbit/Gun.cs
@@ -0,0 +1,18 @@
+﻿namespace XUnitDemo
+{
+  public class Gun
+  {
+    private int ammo = 3;
+
+    public void FireAt(Rabbit rabbit)
+    {
+      if (!HasAmmo()) return;
+      if (rabbit.IsDodging()) rabbit.Miss();
+      else rabbit.Hit();
+      ammo--;
+    }
+
+    public void Recharge() => ammo = 3;
+    public bool HasAmmo() => ammo > 0;
+  }
+}
---
+++ Documents/Schule/4.Klasse/POS/Gun_Rabbit/Rabbit.cs
@@ -0,0 +1,14 @@
+﻿namespace XUnitDemo
+{
+  public class Rabbit
+  {
+    private bool dodging;
+    private bool dead;
+
+    public void Dodge() => dodging = true;
+    public void Hit() => dead = true;
+    public void Miss() => dodging = false;
+    public bool IsDodging() => dodging;
+    public bool IsDead() => dead;
+  }
+}
